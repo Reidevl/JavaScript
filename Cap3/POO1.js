cbc07a214581792828ec2df2a27fa890bf4f918b
Reiwil Lugo <reiwillugo@gmail.com>
2022-06-16 17:59:57 -0400

    Avance 16 Junio

---
+++ Cap3/POO1.js
@@ -0,0 +1,13 @@
+class animal{
+    constructor(especie, edad, color){
+        this.especie = especie;
+        this.edad = edad;
+        this.color = color;
+
+    }
+}
+
+let perro = new animal("perro", 5, "rojo");
+
+// document.write(perro)
+console.log(perro)
