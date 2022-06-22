67036a8967e98f74485c29875ee4a2ecd72fd142
Reiwil Lugo <reiwillugo@gmail.com>
2022-06-21 22:19:01 -0400

    Avances 21 Junio

--- Cap4/String.js
+++ Cap4/String.js
@@ -1,6 +1,17 @@
-let cadena = "ABCDEFG";
-// let cadena2 = "al "
+// let cadena = "   pedro   ";
+// // let cadena2 = "al "
 
-resultado = cadena.substring();
+// resultado = cadena.trimStart();
 
-document.write(resultado); 
+// document.write(resultado.length); 
+
+let numeros = ["abecedario","manzana", "pedro", "dedo", "bobo"];
+
+document.write(numeros + "<br>");
+// let resultado = numeros.reverse();
+
+numeros.splice(-2,0, "roberto", "gustavo", "maximo");
+
+document.write(numeros);
+
+ 
