5cb9fffa174c8dbd3184c06b4e90814ae03871d9
Reiwil Lugo <reiwillugo@gmail.com>
2022-07-12 22:00:16 -0400

    Avances 12 de Julio

--- Cap6/dom.js
+++ Cap6/dom.js
@@ -10,7 +10,18 @@
 
 // document.write(input.type) 
 
-const titulo = document.querySelector(".titulo");
-
+/*Atributo Style
 titulo.style.padding = "30px"; 
 titulo.style.backgroundColor = "#48e"; 
+*/
+
+
+const titulo = document.querySelector(".titulo");
+
+// nombreDeClase = "grande";
+
+// let valor = titulo.classList.contains(nombreDeClase);
+
+titulo.classList.replace("grande", "chico") ;
+
+// document.write(valor)
---
+++ Cap6/obtencion_y_mod_d_elem.js
@@ -0,0 +1,11 @@
+
+
+const titulo = document.querySelector(".titulo")
+
+let resultado = titulo.textContent
+let resultado1 = titulo.innerHTML
+let resultado2 = titulo.outerHTML
+
+alert(resultado)
+alert(resultado1)
+alert(resultado2)
